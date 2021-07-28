909757743699d50c0544ccab38aa5c344db726fd
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.github.com>
2021-07-28 13:47:46 +0530

    initial commit

---
+++ backend/src/main/scala/org/tmt/sample/core/SampleImpl.scala
@@ -0,0 +1,12 @@
+package org.tmt.sample.core
+
+import org.tmt.sample.core.models.{AdminGreetResponse, GreetResponse, UserInfo}
+
+import scala.concurrent.Future
+
+class SampleImpl() {
+  def greeting(userInfo: UserInfo): Future[GreetResponse] = Future.successful(GreetResponse(userInfo))
+
+  def adminGreeting(userInfo: UserInfo): Future[AdminGreetResponse] =
+    Future.successful(AdminGreetResponse(userInfo))
+}
